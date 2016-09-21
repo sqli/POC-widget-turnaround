fcef14705057c72fea345478b426aedae56373a2
Damien GUERIN <dguerin@sqli.com>
2016-09-21 17:36:12 +0200

    Add firebase

--- WIDGET/main.js
+++ WIDGET/main.js
@@ -19,4 +19,33 @@
     loadCss('base');
     loadCss(applicationName);
 
+	
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyBWVyFwFb0WCQ8umii9tyHNlPvVm15OJnA",
+		authDomain: "af-notif-widget-484c6.firebaseapp.com",
+		databaseURL: "https://af-notif-widget-484c6.firebaseio.com",
+		storageBucket: "af-notif-widget-484c6.appspot.com",
+		messagingSenderId: "638447157736"
+	  };
+	firebase.initializeApp(config);
+
+	
+	
+	firebase.database().ref('messages/').on('value', function(snapshot) {
+		snapshot.forEach(function(snap) {
+			$("#messages").append('<p>'+snap.val().text+'</p>');
+			console.log(snap.val().text);
+		});
+	});
+	
+	$('#addBtn').on('click', function() {
+		if ($('#message').val()) {		
+			var newPostKey = firebase.database().ref('messages/').push().key;
+			
+			var updates = {};
+			updates[newPostKey] = {text:$('#message').val()};
+			firebase.database().ref('messages/').update(updates);
+		}
+	});
 })();
